b6c62d98e5e48e3f292e3e3ecc9bb0fec5410fa6
Thomas Tran <thomastran878@gmail.com>
2018-04-24 13:40:10 -0700

    Cleaned up javascript

--- HW3/bookclub.js
+++ HW3/bookclub.js
@@ -4,18 +4,18 @@ function newRequest() {
 
 	var title = document.getElementById("title").value;
 	title = title.trim();
-	title = title.replace(" ","+");
+	title = title.replace(" ", "+");
 
 	var author = document.getElementById("author").value;
 	author = author.trim();
-	author = author.replace(" ","+");
+	author = author.replace(" ", "+");
 
 	var isbn = document.getElementById("isbn").value;
 	isbn = isbn.trim();
-	isbn = isbn.replace("-","");
+	isbn = isbn.replace("-", "");
 
 	// Connects possible query parts with pluses
-	var query = ["",title,author,isbn].reduce(fancyJoin);
+	var query = ["", title, author, isbn].reduce(fancyJoin);
 
 	// The JSONp part.  Query is executed by appending a request for a new
 	// Javascript library to the DOM.  It's URL is the URL for the query. 
@@ -35,109 +35,128 @@ function newRequest() {
 		var beginning = "https://www.googleapis.com/books/v1/volumes?q="
 		var callback = "&callback=handleResponse"
 
-		script.src = beginning+query+callback	
+		script.src = beginning + query + callback
 		script.id = "jsonpCall";
 
 		// put new script into DOM at bottom of body
-		document.body.appendChild(script);	
-		}
+		document.body.appendChild(script);
+	}
 
 }
 
 
 /* Used above, for joining possibly empty strings with pluses */
-function fancyJoin(a,b) {
-    if (a == "") { return b; }	
-    else if (b == "") { return a; }
-    else { return a+"+"+b; }
+function fancyJoin(a, b) {
+	if (a == "") { return b; }
+	else if (b == "") { return a; }
+	else { return a + "+" + b; }
 }
 
-var numtiles=0;
+var numtiles = 0;
 
 /* The callback function, which gets run when the API returns the result of our query */
 function handleResponse(bookListObj) {
 	var bookList = bookListObj.items;
 
-	/* where to put the data on the Web page */ 
+	/* where to put the data on the Web page */
 	var bookDisplay = document.getElementById("bookDisplay");
 	/* write each title as a new paragraph */
-	for (i=0; i<bookList.length; i++) {
+	for (i = 0; i < bookList.length; i++) {
 		var book = bookList[i];
+
+		// Get title
 		var title = book.volumeInfo.title;
+
+		// Get author(s)
 		var author = book.volumeInfo.authors;
+		var author_string = author.join(', ');
+
+		// Get description
 		var desc = book.volumeInfo.description;
-		if(desc !=null)
+		if (desc != null)
 			desc_list = desc.split(" ");
+
+		// Get thumbnail image
 		var thumbnail = book.volumeInfo
-		if (thumbnail.imageLinks == null || thumbnail.imageLinks.thumbnail==null)
-				thumbnail = null;
+		if (thumbnail.imageLinks == null || thumbnail.imageLinks.thumbnail == null)
+			thumbnail = null;
 		else thumbnail = thumbnail.imageLinks.thumbnail;
+
+		// Create tile div
 		var tile = document.createElement("div");
 		tile.className = "tile";
-		tile.id = "tile"+numtiles;
+		tile.id = "tile" + numtiles;
 		numtiles++;
+
+		// Create paragraph for title
 		var titlepar = document.createElement("p");
 		titlepar.textContent = title;
 		titlepar.className = "titlepar";
+
+		// Create paragraph for author(s)
 		var authorpar = document.createElement("p");
-		authorpar.textContent = author;
+		authorpar.textContent = author_string;
 		authorpar.className = "authorpar";
+
+		// Create paragraph for description
 		var descpar = document.createElement("p");
 		descpar.className = "descpar";
-		var wordblock = document.createElement("div");
-		wordblock.className = "wordblock";
+		if (desc != null)
+			descpar.textContent = desc_list.splice(0, 30).join(" ");
+
+		// Create paragraph for Xsymbol
 		var Xsymbol = document.createElement("p");
 		Xsymbol.textContent = '\u24E7';
 		Xsymbol.className = "Xsymbol";
-		if(desc!=null)
-			descpar.textContent = desc_list.splice(0,30).join(" ");
+
+		// Create div to store hold all paragraphs
+		// Goes inside tile div
+		var wordblock = document.createElement("div");
+		wordblock.className = "wordblock";
+
+		// Create image
+		// Goes inside tile div
 		var thumbnailimg = document.createElement("img");
-		if(thumbnail==null)
+		if (thumbnail == null)
 			thumbnailimg.src = "file://null";
 		else
 			thumbnailimg.src = thumbnail;
-		thumbnailimg.alt= title;
-		if(thumbnail == null)
-			thumbnailimg.alt = "no image";
+		thumbnailimg.alt = title;
 		thumbnailimg.className = "thumbnailimg";
+
 		/* ALWAYS AVOID using the innerHTML property */
-		tile.append(thumbnailimg);
-		tile.append(Xsymbol);
-		tile.append(titlepar);
-		tile.append(authorpar);
-		if(descpar!=null)
-			tile.append(descpar);
+		// Assemble wordblock
 		wordblock.appendChild(Xsymbol);
 		wordblock.appendChild(titlepar);
 		wordblock.appendChild(authorpar);
-		wordblock.appendChild(descpar);
+		if (descpar != null)
+			wordblock.appendChild(descpar);
+
+		// Assemble final tile
+		tile.append(thumbnailimg);
 		tile.append(wordblock);
 		bookDisplay.append(tile);
-	}	
+	}
 	addButtonActions();
 }
-function addOnClick(element,func,param)
-{
-	 function noarg(){
+function addOnClick(element, func, param) {
+	function noarg() {
 		func(param);
 	}
 	element.onclick = noarg;
 }
 
-function addButtonActions()
-{
+function addButtonActions() {
 	var tile_list = document.getElementsByClassName("tile");
-	for(let i=0;i<tile_list.length;i++)
-	{
+	for (let i = 0; i < tile_list.length; i++) {
 		var our_tile = tile_list[i];
-		 var X_button = our_tile.getElementsByClassName("Xsymbol");
-		 var tile_ID = our_tile.id;
-		addOnClick(X_button[0],removeElement,tile_ID);
+		var X_button = our_tile.getElementsByClassName("Xsymbol");
+		var tile_ID = our_tile.id;
+		addOnClick(X_button[0], removeElement, tile_ID);
 	}
 }
 
-function removeElement(tile)
-{
+function removeElement(tile) {
 	var tile_div = document.getElementById(tile);
 	var bookDisplay = document.getElementById("bookDisplay");
 	bookDisplay.removeChild(tile_div);
